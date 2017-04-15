d350d066abe8e0f7a1e8d7e1ff6ce6c3894ee0d3
Oleg Katkov <lezh1k.vohrer@gmail.com>
2017-04-15 22:31:10 +0600

    First commit

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+ * ChronoButtons.c
+ *
+ * Created: 21:49 12.04.2017
+ * Author : Lezh1k
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+int
+main(void) {
+  while(1) {
+
+  }
+}
+//////////////////////////////////////////////////////////////////////////

d203bb2630201390334b28799abe7b7ed527fd64
Oleg Katkov <lezh1k.vohrer@gmail.com>
2017-05-06 01:08:09 +0600

    Added beeps on start

--- Chronometer/include/MainWindow.h
+++ Chronometer/include/MainWindow.h
@@ -11,6 +11,7 @@ class CChronometerController;
 class QTimer;
 class QSerialPort;
 class QStandardItemModel;
+class QMediaPlayer;
 
 class MainWindow : public QMainWindow
 {
@@ -24,7 +25,13 @@ private:
   Ui::MainWindow *ui;
   CChronometerController* m_chronometer_controller;
   QTimer* m_refresh_timer;  
+  QTimer* m_start_timer;
   QStandardItemModel* m_model_ports;
+
+  QMediaPlayer* m_start_player;
+  int8_t m_signals_count;
+
+  void play_start_sound();
 
 private slots:
   void btn_start_stop_released();
@@ -35,6 +42,7 @@ private slots:
   void btn_fall0_released();
   void btn_fall1_released();
 
+  void start_timer_timeout();
 };
 
 #endif // MAINWINDOW_H
--- Chronometer/src/MainWindow.cpp
+++ Chronometer/src/MainWindow.cpp
@@ -14,18 +14,29 @@
 #include <QPushButton>
 #include <QComboBox>
 
+#include <QApplication>
+#include <QDir>
+#include <QFile>
+#include <QThread>
+#include <QMediaPlayer>
+
+
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent),
   ui(new Ui::MainWindow),
   m_chronometer_controller(nullptr),
   m_refresh_timer(nullptr),
-  m_model_ports(nullptr) {
+  m_model_ports(nullptr),
+  m_start_player(nullptr){
 
   ui->setupUi(this);
   m_chronometer_controller = new CChronometerController;
   m_refresh_timer = new QTimer(this);
   m_refresh_timer->setInterval(10);
+  m_start_timer = new QTimer(this);
+  m_start_timer->setInterval(1000);
   ui->lbl_error->setVisible(true);
+  m_start_player = new QMediaPlayer;
 
   m_model_ports = new QStandardItemModel;
   QList<QSerialPortInfo> lst_ports = QSerialPortInfo::availablePorts();
@@ -37,6 +48,8 @@ MainWindow::MainWindow(QWidget *parent) :
 
   connect(m_refresh_timer, &QTimer::timeout,
           this, &MainWindow::refresh_timer_timeout);
+  connect(m_start_timer, &QTimer::timeout,
+          this, &MainWindow::start_timer_timeout);
   connect(ui->btn_start_stop, &QPushButton::released,
           this, &MainWindow::btn_start_stop_released);
   connect(ui->cb_serial_ports, (void(QComboBox::*)(int)) &QComboBox::currentIndexChanged,
@@ -54,18 +67,44 @@ MainWindow::MainWindow(QWidget *parent) :
 
 MainWindow::~MainWindow() {
   if (m_chronometer_controller) delete m_chronometer_controller;
+  if (m_model_ports) delete m_model_ports;
   delete ui;
+}
+//////////////////////////////////////////////////////////////
+
+
+static const QString beep1 = "beep1.wav";
+static const QString beep2 = "beep2.wav";
+
+void
+MainWindow::play_start_sound() {
+
+  QString dp = QApplication::applicationDirPath();
+  QDir dir(dp + QDir::separator() + "resources");
+  if (!dir.exists()) {
+    ui->lbl_error->setText("resources directory doesn't exist");
+    return;
+  }
+
+  QString files[2] = {beep1, beep2};
+  for (int i = 0; i < 2; ++i) {
+    QFile f(dir.path() + QDir::separator() + files[i]);
+    if (!f.exists()) {
+      ui->lbl_error->setText(QString("%1 doesn't exist").arg(files[i]));
+      return;
+    }
+  }
+
+  m_signals_count = 3;
+  m_start_timer->start();
 }
 //////////////////////////////////////////////////////////////
 
 void
 MainWindow::btn_start_stop_released() {
-  QString start_str;
+
   if (!m_chronometer_controller->is_running()) {
-    if (!m_chronometer_controller->start(start_str)) {
-      ui->lbl_error->setVisible(true);
-      ui->lbl_error->setText(start_str);
-    }
+    play_start_sound();
   } else {
     m_chronometer_controller->stop_all();    
   }
@@ -134,5 +173,26 @@ void
 MainWindow::btn_fall1_released() {
   m_chronometer_controller->fall1();
   refresh_timer_timeout();
+}
+//////////////////////////////////////////////////////////////
+
+void
+MainWindow::start_timer_timeout() {
+  QString dp = QApplication::applicationDirPath();
+  QDir dir(dp + QDir::separator() + "resources");
+  --m_signals_count;
+  if (m_signals_count > 0) {
+    m_start_player->setMedia(QUrl::fromLocalFile(dir.path() + QDir::separator() + beep1));
+    m_start_player->play();
+  } else {
+    QString start_str;
+    if (!m_chronometer_controller->start(start_str)) {
+      ui->lbl_error->setVisible(true);
+      ui->lbl_error->setText(start_str);
+    }
+    m_start_timer->stop();
+    m_start_player->setMedia(QUrl::fromLocalFile(dir.path() + QDir::separator() + beep2));
+    m_start_player->play();
+  }
 }
 //////////////////////////////////////////////////////////////
